c7227e56db88c40456471ca3f5593e417d669ee6
cwmk9999@naver.com <>
2026-05-14 17:31:32 +0900

    ai챗봇 api연결 추후 멀티턴 후 RAG 연결

--- src/main/java/kr/or/saeroi/AiChatbot/aiChatController.java
+++ src/main/java/kr/or/saeroi/AiChatbot/aiChatController.java
@@ -1,8 +1,13 @@
 package kr.or.saeroi.AiChatbot;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class aiChatController {
@@ -14,5 +19,13 @@ public class aiChatController {
 	public String chat() {
 		
 		return "chat";
+	}
+
+	@RequestMapping(value = "/gemini", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	@ResponseBody
+	public String gemini(@RequestBody Map<String, String> params) {
+		String prompt = params.get("prompt");
+		System.out.println(prompt);
+		return aiChatservice.getChatResponse(prompt);
 	}
 }
--- src/main/webapp/WEB-INF/views/chat.jsp
+++ src/main/webapp/WEB-INF/views/chat.jsp
@@ -7,7 +7,34 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<input id="chat" type="text">
+	<input id="prompt" type="text" name="prompt">
 	<button id="btn" type="button" > 전송 </button>
+	<div id="chat"></div>
+	<script>
+		document.querySelector('#btn').
+			addEventListener('click', async function(){
+				let chat = document.querySelector('#chat');
+				let prompt = document.querySelector('#prompt');
+				let url = "gemini";
+				let param = {
+					prompt:prompt.value
+				}
+				let option = {
+					method:"POST",
+					headers:{
+						'Content-Type':'application/json'
+					},
+					body:JSON.stringify(param)	
+				}
+				let response = await fetch(url,option);
+				let data = await response.json()
+				let aiResponse = data.candidates[0].content.parts[0].text;
+				chat.innerHTML += aiResponse
+				prompt.value='';
+				
+		})
+			
+		
+	</script>
 </body>
 </html>
