85b20c50911500f2d926cbb4e06d31ce219619a7
cwmk9999@naver.com <>
2026-05-15 15:58:10 +0900

    풀받으려고 커밋

--- src/main/java/kr/or/saeroi/AiChatbot/DbTool.java
+++ src/main/java/kr/or/saeroi/AiChatbot/DbTool.java
@@ -1,5 +1,21 @@
 package kr.or.saeroi.AiChatbot;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dev.langchain4j.agent.tool.Tool;
+import kr.or.saeroi.dao.QualityDAO;
+
 public class DbTool {
 
+	@Autowired
+	private QualityDAO qualityDAO;
+	
+	@Tool("이차전지 공장의 특정 생산 라인의 현재 배터리 재고를 조회합니다.")
+	public String getLineStatus(String lineName) {
+		
+		Map<String, Object> data = qualityDAO.selectLineStatus(lineName);
+		return data.toString();
+	}
 }
