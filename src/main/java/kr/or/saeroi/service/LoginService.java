192a245cc91aef4c99b835f41d11834dc3e79821
whtjdfo35@gmail.com <>
2026-05-18 10:48:06 +0900

    로그인 페이지 재 업로드

--- src/main/java/kr/or/saeroi/service/LoginService.java
+++ src/main/java/kr/or/saeroi/service/LoginService.java
@@ -1,11 +1,14 @@
 package kr.or.saeroi.service;
 
 import java.sql.Connection;
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import kr.or.saeroi.dao.LoginDAO;
 import kr.or.saeroi.dto.LoginDTO;
@@ -18,6 +21,9 @@ public class LoginService {
 
     @Autowired
     private DataSource dataSource;
+    
+    @Autowired
+    private JavaMailSender mailSender;
 
     public LoginDTO login(String empno, String pw) {
 
@@ -26,7 +32,7 @@ public class LoginService {
         try {
             conn = dataSource.getConnection();
 
-            LoginDTO login = loginDAO.FindEmpNo(conn, empno);
+            LoginDTO login = loginDAO.Find_empno(conn, empno);
 
             if(login == null) {
                 return null;
@@ -55,12 +61,20 @@ public class LoginService {
 
 	
 	public String reset_pw(String empno) {
-		// TODO Auto-generated method stub
-		return null;
+		String temp_pw = UUID.randomUUID().toString().substring(0, 8);
+
+	    loginDAO.update_pw(null, empno, temp_pw);
+
+	    return temp_pw;		
 	}
 
 	public void send_temp_pw(String email, String tempPw) {
-		// TODO Auto-generated method stub
 		
+		SimpleMailMessage msg = new SimpleMailMessage();
+	    msg.setTo(email);
+	    msg.setSubject("임시 비밀번호 안내");
+	    msg.setText("임시 비밀번호는 [" + tempPw + "] 입니다.");
+
+	    mailSender.send(msg);
 	}
 }
