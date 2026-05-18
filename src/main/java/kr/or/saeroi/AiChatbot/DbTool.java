bcc79be6d2e2fde14fd199c1ec67d30466e28894
cwmk9999@naver.com <>
2026-05-18 16:18:59 +0900

    오류해결

--- src/main/java/kr/or/saeroi/AiChatbot/DbTool.java
+++ src/main/java/kr/or/saeroi/AiChatbot/DbTool.java
@@ -55,12 +55,10 @@ public class DbTool {
 		}
 	}
 	
-	@Tool("입출고 기록을 조회합니다.시작행(startRow) 종료행(endRow)을 언급하지않았다면 무리하게 채우지 말고 반드시 빈 문자열(\"\")로 넘겨주어야 합니다." +
-			"만약 조회 시작일(startDate)과 종료일(endDate)을 언급하지 않았다면, 무리하게 채우지 말고 반드시 빈 문자열(\"\")로 넘겨주어야 합니다." +
+	@Tool("만약 조회 시작일(startDate)과 종료일(endDate)을 언급하지 않았다면, 무리하게 채우지 말고 반드시 빈 문자열(\"\")로 넘겨주어야 합니다." +
 			"사용자가 특정 품목이나 검색어를 언급하면 keyword에 넣고, searchType은 '품목명' 등으로 지정합니다."+
 			"사용자가 '출고 기록' 혹은 '입고 기록'을 요청하면 keyword에 넣지 말고, 반드시 searchType에 '구분'을 넣고 keyword에는 '출고' 또는 '입고'를 넣으세요.")
-	public String getInOut(int startRow,
-							int endRow,
+	public String getInOut(
 							String searchType,
 							String keyword,
 							String startDate,
@@ -68,7 +66,7 @@ public class DbTool {
 		String type = (searchType != null && !searchType.isEmpty()) ? searchType : null; 
 		String key = (keyword != null && !keyword.isEmpty()) ? keyword : null; 
 		
-		List<InoutDTO> list = inoutDAO.selectInoutList(startRow, endRow, type, key,startDate,endDate);
+		List<InoutDTO> list = inoutDAO.selectInoutList(type, key,startDate,endDate);
 		return list.isEmpty() ? "해당 조건으로 조회된 입출고 기록이 없습니다." :list.toString();
 	}
 }
