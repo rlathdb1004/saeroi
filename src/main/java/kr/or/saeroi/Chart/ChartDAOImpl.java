3718046fc9a5176d4a496aa380eaf34ea6442a2b
cwmk9999@naver.com <>
2026-05-22 01:41:10 +0900

    차트 오류 수정

--- src/main/java/kr/or/saeroi/Chart/ChartDAOImpl.java
+++ src/main/java/kr/or/saeroi/Chart/ChartDAOImpl.java
@@ -1,5 +1,6 @@
 package kr.or.saeroi.Chart;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,10 +14,20 @@ public class ChartDAOImpl implements ChartDAO{
 	@Autowired
 	SqlSession sqlSession;
 	
-	public List<Map<String, Object>> chartday(String searchType){
-		
-		List<Map<String, Object>> list = sqlSession.selectList("mapper.chart.sleect_day_data",searchType);
-	
+	public List<Map<String, Object>> chartday(String searchType,String searchItem){
+		Map<String, Object> paramMap = new HashMap<>();
+		System.out.println(searchItem);		
+		paramMap.put("searchType", searchType);
+	    paramMap.put("searchItem", searchItem);
+	    
+		List<Map<String, Object>> list = sqlSession.selectList("mapper.chart.sleect_day_data",paramMap);
+	System.out.println("doalist"+list);
 		return list;
 	};
+	
+	public List<Map<String, Object>> itemList(){
+		List<Map<String, Object>> list = sqlSession.selectList("mapper.chart.sleect_item");
+		
+		return list;
+	}
 }
